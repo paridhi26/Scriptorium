258bed0e75e31112d21a562857c5a4b5640ad915
antijun <davidwu361@gmail.com>
2024-11-04 15:48:15 -0500

    changed relative import paths to absolute path

--- PP1/my-app/pages/api/auth/signup.js
+++ PP1/my-app/pages/api/auth/signup.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import prisma from '../../../lib/prisma';
+import prisma from '@/lib/prisma';
 
 export default async function signUp(req, res) {
     if (req.method !== 'POST') {
