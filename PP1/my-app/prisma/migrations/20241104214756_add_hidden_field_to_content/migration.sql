76b4bf19792c63d8c2b1a533d59ac5a1f7447838
antijun <davidwu361@gmail.com>
2024-11-04 16:48:43 -0500

    updated schema for hiding content + started files for content report use cases

---
+++ PP1/my-app/prisma/migrations/20241104214756_add_hidden_field_to_content/migration.sql
@@ -0,0 +1,34 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_BlogPost" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "hidden" BOOLEAN NOT NULL DEFAULT false,
+    CONSTRAINT "BlogPost_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_BlogPost" ("content", "createdAt", "description", "id", "title", "updatedAt", "userId") SELECT "content", "createdAt", "description", "id", "title", "updatedAt", "userId" FROM "BlogPost";
+DROP TABLE "BlogPost";
+ALTER TABLE "new_BlogPost" RENAME TO "BlogPost";
+CREATE TABLE "new_Comment" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+    "blogPostId" INTEGER,
+    "parentCommentId" INTEGER,
+    "hidden" BOOLEAN NOT NULL DEFAULT false,
+    CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Comment_blogPostId_fkey" FOREIGN KEY ("blogPostId") REFERENCES "BlogPost" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Comment_parentCommentId_fkey" FOREIGN KEY ("parentCommentId") REFERENCES "Comment" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Comment" ("blogPostId", "content", "createdAt", "id", "parentCommentId", "userId") SELECT "blogPostId", "content", "createdAt", "id", "parentCommentId", "userId" FROM "Comment";
+DROP TABLE "Comment";
+ALTER TABLE "new_Comment" RENAME TO "Comment";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
