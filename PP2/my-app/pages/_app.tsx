634a52f9870c78e8a42bfcb99e35bf4e6b60821e
paridhi <paridhi.goel@mail.utoronto.ca>
2024-11-26 17:14:53 -0500

    added Layout tag here so we don't have to everywhere else

--- PP2/my-app/pages/_app.tsx
+++ PP2/my-app/pages/_app.tsx
@@ -2,9 +2,11 @@
 import React from "react";
 import "../styles/globals.css"; // Import global CSS (if any)
 import { AppProps } from "next/app"; // Type for app props
+import Layout from '../components/Layout';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+
+  return <Layout><Component {...pageProps} /></Layout>;
 };
 
 export default MyApp;
